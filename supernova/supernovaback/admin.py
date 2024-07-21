3334ebfff14c02c88c4e98f32a335d53b367a319
RyuSeongHo <ryuseongho7@naver.com>
2024-07-21 23:39:20 +0900

    feat: added submit and pet_select

--- supernova/supernovaback/admin.py
+++ supernova/supernovaback/admin.py
@@ -5,9 +5,11 @@ from .models import User  # User 모델을 import합니다.
 from .models import Semester  # Semester 모델을 import합니다.
 from .models import Quiz
 from .models import TimeSlot
+from .models import Answer
 
 # User 모델을 admin 사이트에 등록합니다.
 admin.site.register(User)
 admin.site.register(Semester)
 admin.site.register(Quiz)
 admin.site.register(TimeSlot)
+admin.site.register(Answer)
